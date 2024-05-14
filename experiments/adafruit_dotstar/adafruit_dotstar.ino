c6f6c5095cd3884eba24a5a0613c7f6db14d3d5c
Daniel Mateos <daniel@mateos.io>
2024-05-14 13:27:22 +0200

    Minimal working dotstar example

--- experiments/adafruit_dotstar/adafruit_dotstar.ino
+++ experiments/adafruit_dotstar/adafruit_dotstar.ino
@@ -1,10 +1,9 @@
 #include <Arduino.h>
 #include <Adafruit_DotStar.h>
-#include <wiring_private.h>
 
 #define NUMPIXELS 16    // Number of LEDs in strip
-#define STRIP_DATA 14   // MOSI1 is defined as 14ul in variant.h, but software SPI doesn't work when I switch up the pins!! Why the hell no?.
-#define STRIP_CLOCK 15  // SCK1 sane
+#define STRIP_DATA PIN_SPI1_MOSI   // MOSI1 is defined as 14ul in variant.h, but software SPI doesn't work when I switch up the pins!! Why the hell no?. -> I was switching the pins wrong (doh!)
+#define STRIP_CLOCK PIN_SPI1_SCK  // Defined in variant.h
 
 // Status indicator LED
 #define LED_R 0
@@ -18,31 +17,24 @@
 // Sensor
 #define HALL 7
 
-
 // Setting up sercom 2 for hardware SPI.
 // Pads 0 and 1 are not a pin in SAMD21E18A.
 SPIClass sercom2SPI(&sercom2, -1, STRIP_DATA, STRIP_CLOCK, SPI_PAD_2_SCK_3, SERCOM_RX_PAD_0);
 
-// Adafruit_DotStar strip(NUMPIXELS, STRIP_DATA, STRIP_CLOCK, DOTSTAR_BGR);  // Software SPI (???)
-Adafruit_DotStar strip(NUMPIXELS, DOTSTAR_BGR, &sercom2SPI); // Hardware SPI (??)
+// Adafruit_DotStar strip(NUMPIXELS, STRIP_DATA, STRIP_CLOCK, DOTSTAR_BGR);  // Software SPI
+Adafruit_DotStar strip(NUMPIXELS, DOTSTAR_BGR, &sercom2SPI);
 
 const int allOutputPins[] = { STRIP_DATA, STRIP_CLOCK, LED_R, LED_G, LED_B };
-
-
 
 void setup() {
   for (int i = 0; i < 5; i++) {
     pinMode(allOutputPins[i], OUTPUT);
   }
 
-  pinPeripheral(STRIP_CLOCK, PIO_SERCOM);  // needs #include <wiring_private.h>
-  pinPeripheral(STRIP_DATA, PIO_SERCOM);   // needs #include <wiring_private.h>
-
-
   pinMode(HALL, INPUT);
 
   strip.begin();  // Initialize pins for output
-  strip.show();  // Turn all LEDs off ASAP
+  strip.show();   // Turn all LEDs off ASAP
   strip.setBrightness(20);
 
   digitalWrite(LED_R, HIGH);
@@ -60,10 +52,13 @@ void loop() {
   strip.show();
   Serial.println(micros() - now);
   delay(1500);
+
   for (int i = 0; i < NUMPIXELS; i++) {
     strip.setPixelColor(i, 0);
   }
+  now = micros();
   strip.show();
+  Serial.println(micros() - now);
+
   delay(1500);
-  Serial.println(SPI_INTERFACES_COUNT);  // Tools menu-> Serial config
 }
