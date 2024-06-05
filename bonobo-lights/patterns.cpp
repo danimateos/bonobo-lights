b7fa4d0efa74d76b95ce6cf41d9aeca4c2594777
Daniel Mateos <daniel@mateos.io>
2024-06-05 16:39:32 +0200

    Add new patterns and saturate some existing ones

--- bonobo-lights/patterns.cpp
+++ bonobo-lights/patterns.cpp
@@ -12,4 +12,4 @@ Pattern dudemabaike = {
   60, 16, 3, false, (uint8_t *) &dudemabaikeData
 };
 
-extern const uint8_t* all240patterns[N_PATTERNS] = {coolData, trianglesData, rainbowGradientData, mafaceData, dragonData, artichokeData, easterEggData};
+extern const uint8_t* all240patterns[N_PATTERNS] = { bonoboData, coolData, rainbowGradientData, mafaceData, trianglesData, dragonData, artichokeData,flagData, easterEggData};
