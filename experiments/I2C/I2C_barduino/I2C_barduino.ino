54661df009d04b8a33c256ca148487807312476f
Daniel Mateos <daniel@mateos.io>
2024-06-20 16:01:46 +0200

    Connection between hub and spoke

--- experiments/I2C/I2C_barduino/I2C_barduino.ino
+++ experiments/I2C/I2C_barduino/I2C_barduino.ino
@@ -7,7 +7,7 @@ const int bufferSize = 50;  // Define the size of your buffer
 char buffer[bufferSize];    // Create the character buffer
 char terminator = '\n';     // Define the terminator character
 int bytesRead = 0;          // Variable to hold the number of bytes read
-
+bool readPending = false;
 
 void setup() {
   Wire.begin();
@@ -15,18 +15,53 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("waiting");
-  delay(666);
+  Serial.print(".");
+  delay(1000);
+  Serial.print(".");
+  delay(1000);
+  Serial.println(".");
+  delay(1000);
 
   if (Serial.available() > 0) {
     bytesRead = Serial.readBytesUntil('\n', buffer, bufferSize - 1);
     // Null-terminate the buffer to make it a valid C string
     buffer[bytesRead] = '\0';
-
+    Serial.print("Read from Serial: ");
     Serial.println(buffer);
 
     Wire.beginTransmission(ADDRESS);
     Wire.write(buffer);
     Wire.endTransmission();
+    memset(buffer, 0, bufferSize);
+    readPending = true;
   }
+
+
+
+
+
+  if (readPending) {
+    Serial.println("Proceeding to request echo");
+    Wire.requestFrom(ADDRESS, bytesRead);
+
+
+    if (Wire.available()) {
+      size_t i = 0;
+      while (Wire.available()) {
+        buffer[i++] = Wire.read();
+      }
+      // Null-terminate the received data to make it a valid C string
+      buffer[i] = '\0';
+
+      Serial.println("Received response:");
+      Serial.println(buffer);
+      // Clear the buffer after reading
+      memset(buffer, 0, bufferSize);
+      readPending = false;
+    } else {
+      Serial.println("No response received");
+    }
+  }
+
+  delay(500);
 }
--- experiments/I2C/I2C_board_03/I2C_board_03.ino
+++ experiments/I2C/I2C_board_03/I2C_board_03.ino
@@ -4,31 +4,42 @@
 // Status indicator LED
 #define LED_R 0
 #define LED_G 1
-#define LED_B 2
 
 // Human input
-#define BUTTON_1 3
+#define BUTTON_1 7
 #define BUTTON_2 6
 
 // Sensor
-#define HALL 7
+#define HALL 3
 
 #define ADDRESS 97  // Slave; address randomly chosen
 
 // Variables
 const int bufferSize = 50;  // Define the size of your buffer
 char buffer[bufferSize];    // Create the character buffer
+bool pendingTransmission = false;
 
 void setup() {
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+
+  pinMode(BUTTON_1, INPUT_PULLUP);
+  pinMode(BUTTON_2, INPUT_PULLUP);
+
   Serial.begin(115200);
 
-  Wire.begin(ADDRESS);  
+  Wire.begin(ADDRESS);
   Wire.onReceive(receive);
+  Wire.onRequest(respond);
 }
 
 void loop() {
   Serial.print(".");
-  delay(500);
+  delay(1000);
+  Serial.print(".");
+  delay(1000);
+  Serial.println(".");
+  delay(1000);
 }
 
 void receive(int bytesToRead) {
@@ -38,5 +49,26 @@ void receive(int bytesToRead) {
     buffer[i] = Wire.read();  // Read each byte into the buffer
     i++;
   }
+
+
+  buffer[bytesToRead] = '\0';
+  pendingTransmission = true;
+
+  Serial.print("Received through Wire: ");
   Serial.println(buffer);
+}
+
+void respond() {
+  Serial.println("Master requested data");
+
+  // Send the prepared response to the master
+  if (pendingTransmission) {
+    Wire.write(buffer);
+
+    Serial.print("Sent to master: ");
+    Serial.println(buffer);
+  }
+
+  memset(buffer, 0, bufferSize);
+  pendingTransmission = false;
 }
