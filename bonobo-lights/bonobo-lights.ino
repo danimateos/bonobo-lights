59b5b7d1b46d2399eeaaffbef0f67975f9510a74
Daniel Mateos <daniel@mateos.io>
2024-12-23 16:40:35 +0100

    Add cute startup sequence with onboard LEDs

--- bonobo-lights/bonobo-lights.ino
+++ bonobo-lights/bonobo-lights.ino
@@ -13,6 +13,7 @@ void setup() {
   pinMode(STRIP_CLOCK, OUTPUT);
   pinMode(LED_R, OUTPUT);
   pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
 
   pinMode(HALL, INPUT);
   pinMode(BUTTON_1, INPUT_PULLUP);
@@ -25,6 +26,9 @@ void setup() {
   Wire.onReceive(readI2C);
 
   Serial.begin(115200);
+
+  startupGreeting();
+
   sliceOnMicros = micros();
 
 
@@ -45,4 +49,22 @@ void loop() {
   printSerial();
 
   step += 1;
+}
+
+void startupGreeting() {
+
+  for (int i = 0; i < 2; i++) {
+    digitalWrite((LED_R + i) % 3, HIGH);
+    digitalWrite((LED_G + i + 1) % 3, LOW);
+    digitalWrite((LED_B + i + 2) % 3, LOW);
+    delay(100);
+  }
+
+  digitalWrite(LED_R, HIGH);
+  digitalWrite(LED_G, HIGH);
+  digitalWrite(LED_B, HIGH);
+  delay(100);
+  digitalWrite(LED_R, LOW);
+  digitalWrite(LED_G, LOW);
+  digitalWrite(LED_B, LOW);
 }
